e3cb0b30c01503b3a87eab72fd4b12177adb3931
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-06-30 14:42:29 +0700

    up

---
+++ Backend/backend/src/main/java/com/example/backend/entity/User.java
@@ -0,0 +1,64 @@
+package com.example.backend.entity;
+
+import java.time.LocalDateTime;
+
+import javax.management.relation.Role;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(
+    name = "users",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_users_email", columnNames = "email")
+    }
+)
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String fullName;
+
+    @Column(nullable = false, length = 160)
+    private String email;
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean enabled = true;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate(){
+        LocalDateTime now = LocalDateTime.now();
+
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+    
+}
